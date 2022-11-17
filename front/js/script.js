4f9b6e0f0e7e077977226f1484cac89dbfbb93b2
Blanc Guillaume <guillaume.blanc92@hotmail.fr>
2022-11-17 03:54:55 +0100

    fetch data ..

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,16 @@
 fetch ("http://localhost:3000/api/products")
 .then((res) => res.json())
-.then((data) => console.log(data))
+.then((data) =>{
+    const imageUrl =data[1].imageUrl
+ console.log("url de l'image, imageUrl")
+ const anchor = document.createElement("a");
+anchor.href = "http://localhost:3000/images/kanap02.jpeg"
+anchor.text ="un super canap"
+const items=  document.querySelector("#items")
+items.appendChild(anchor)
+console.log("Nous avons ajouté le lien")
+})
+
+
+
+
