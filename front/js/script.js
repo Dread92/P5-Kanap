95833131a3ac264bd2a71058573df294b72af1e0
Blanc Guillaume <guillaume.blanc92@hotmail.fr>
2022-11-17 04:04:28 +0100

    js refactoring

--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,23 @@
 fetch ("http://localhost:3000/api/products")
 .then((res) => res.json())
-.then((data) =>{
-    const imageUrl =data[1].imageUrl
- console.log("url de l'image, imageUrl")
- const anchor = document.createElement("a");
+.then((data) => getProduct(data))
+
+
+
+function getProduct(dataproduct){
+console.log(dataproduct)
+const imageUrl =dataproduct[1].imageUrl
+console.log("url de l'image, imageUrl")
+
+
+const anchor = document.createElement("a");
 anchor.href = "http://localhost:3000/images/kanap02.jpeg"
 anchor.text ="un super canap"
 const items=  document.querySelector("#items")
-items.appendChild(anchor)
-console.log("Nous avons ajouté le lien")
-})
+
+    items.appendChild(anchor)
+    console.log("Nous avons ajouté le lien")
+    }
 
 
 
