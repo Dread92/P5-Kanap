b4b51f19edd6c651e3a4fcb1b24d68c82f79cf92
Blanc Guillaume <guillaume.blanc92@hotmail.fr>
2022-11-20 18:37:11 +0100

    removing console logs...

--- front/js/script.js
+++ front/js/script.js
@@ -5,18 +5,12 @@ fetch ("http://localhost:3000/api/products")
 
 
 function getProduct(dataproduct){
-console.log(dataproduct)
 const imageUrl =dataproduct[1].imageUrl
-console.log("url de l'image, imageUrl")
-
-
 const anchor = document.createElement("a");
 anchor.href = "http://localhost:3000/images/kanap02.jpeg"
 anchor.text ="un super canap"
 const items=  document.querySelector("#items")
-
     items.appendChild(anchor)
-    console.log("Nous avons ajouté le lien")
     }
 
 
