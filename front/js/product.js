02d7c705852c547799ec88a4b3d5249554793414
Blanc Guillaume <guillaume.blanc92@hotmail.fr>
2022-11-29 01:26:22 +0100

    editing minor bug in fetch

--- front/js/product.js
+++ front/js/product.js
@@ -4,8 +4,8 @@
     window.location.search
 */ 
 const queryString = window.location.search
-const parametreURL = new URLSearchParams(queryString); //Recupere la 'queryString' de l'URL
-const  id =  urlParameters.get("id"); // Recupere la valeur de 'id' dans l'URL
+const parametreUrl = new URLSearchParams(queryString); //Recupere la 'queryString' de l'URL
+const  id =  parametreUrl.get("id"); // Recupere la valeur de 'id' dans l'URL
 console.log(id)
 
 fetch(`http://localhost:3000/api/products/${id}`)
