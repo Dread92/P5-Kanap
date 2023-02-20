539bc988e8060fac4665c18cf25a9e0a1ce2bd8f
BLANC_GUILLAUME <guillaume.blanc92@hotmail.fr>
2023-02-21 00:08:38 +0200

    commentaire sur script.js

--- front/js/script.js
+++ front/js/script.js
@@ -14,9 +14,10 @@ fetch("http://localhost:3000/api/products")
       console.log("Message d'erreur : \n" + erreur);
     });
     	
+    //titre de la page
     document.title = ` Bienvenue chez Kanap`
 
-    // Données que l'on veut récupérer dans l'API: 
+    // Données que l'on veut récupérer dans l'API, exemple:
     /* altTxt: "Photo d'un canapé bleu, deux places"
 colors:(3) ['Blue', 'White', 'Black']
 description:"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -25,74 +26,65 @@ name:"Kanap Sinopé"
 price :1849
 _id:"107fb5b75607497b96722bda5b504926" */
 
-/* Fonctions pour récupérer l'id de l'article + création d'un lien qui renvoie vers la page produit .. */
+
+//Fonction globale d'affichage des produits via "data"
     function showProducts(datas) {
-
       if(!datas) return;
-      for (let i = 0; i < datas.length; i++){ /*loop pour prendre chaque produit , accolade englobant toutes les constantes */
-
+      for (let i = 0; i < datas.length; i++){ // boucle qui parcourt le tableau de produits
+      //Pour chaque produit, on récupète les différentes données inhérentes à celui ci via des constantes
       const id = datas[i]._id
       const imageUrl = datas[i].imageUrl
       const altTxt = datas[i].altTxt
       const name = datas[i].name
       const description = datas[i].description
-      /* const {_id, imageUrl, altTxt, name, description} = data[i]     --> destructuring */
-
-   
-     
-      
       const link = makeLink (id)
-      const article = document.createElement('article') /* la fonction make article n'avait qu'une ligne */
+      // Puis on crée les éléments HTML nécessaires pour afficher les informations du produit 
+      const article = document.createElement('article') 
       const image = createImage(imageUrl, altTxt)
       const h3 = createTitle (name)
       const p = createParagraph(description)
-
       /* création d'enfants à <article> */
       article.appendChild(image)
       article.appendChild(h3)
       article.appendChild(p)
-
       appendArticleToLink(link, article)
     }
-
   }
 
 
-
+// Avec id en argument, on crée une balise <a> qui renvoi à  ./product.html?id= + l'id du produit souhaité
     function makeLink (id) {
-
       const link = document.createElement('a')
       link.href = "./product.html?id=" + id
       return link
-
     }
 
+  
+      // On prend la classe items dans le HTML pour ajouter les éléments lien et l'article au DOM
       function appendArticleToLink(link, article) {
-
         const items =  document.querySelector("#items")
         if (items != null) {
           items.appendChild(link)
           link.appendChild(article)
         }
-
       }
 
    
-      /* fabrication de la carte image+ titre + paragraphe */
+      //fabrication de la carte image+ titre + paragraphe 
       function createImage ( imageUrl, altTxt) {
         const image = document.createElement("img")
         image.src = imageUrl 
         image.alt = altTxt
         return image
       }
-
+      // fabrication du titre en récupérant dans le HTML la classe productname et création de l'élément h3
       function createTitle(name) {
         const h3 =  document.createElement("h3")
         h3.textContent = name 
         h3.classList.add('productname')
         return h3
       }
-
+      // fabrication du paragraphe + récupération de l'élément productDescription
       function createParagraph(description){
         const p = document.createElement('p')
         p.textContent = description
